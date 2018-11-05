c80dc596a94d09bf32ccc74952f20f55d3c87ece
Jakob Skrede <jakob2407@gmail.com>
2018-11-05 13:26:55 +0100

    sad

---
+++ no/uio/ifi/asp/runtime/RuntimeStringValue.java
@@ -0,0 +1,29 @@
+package no.uio.ifi.asp.runtime;
+
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.parser.AspSyntax;
+
+public class RuntimeStringValue extends RuntimeValue {
+  String value;
+
+  pubic RuntimeStringValue(String v) {
+    value = v;
+  }
+
+  public String toString(){
+    return value;
+  }
+
+  public String getStringValue(String what, AspSyntax where) {
+    return value;
+  }
+
+  public RuntimeValue evalAdd(RuntimeValue v, AspSyntax where) {
+    if (v instanceof RuntimeStringValue) {
+      return new RuntimeStringValue(value + v.getStringValue("string", where));
+    }
+
+    runtimeError("Type error for +", where);
+    return null;
+  }
+}
