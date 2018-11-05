b62a553092139086a25ec6b185ce8f063c6ac18a
sigurson <sigurd.so@gmail.com>
2018-11-05 13:36:09 +0100

    iiiii

---
+++ no/uio/ifi/asp/runtime/RuntimeFloatValue.java
@@ -0,0 +1,97 @@
+package no.uio.ifi.asp.runtime;
+
+import no.uio.ifi.asp.main.*;
+import no.uio.ifi.asp.parser.AspSyntax;
+
+public class RuntimeFloatValue extends RuntimeValue {
+  double floatValue;
+
+
+  public RuntimeFloatValue(double v) {
+    this.floatValue = v;
+  }
+
+	public double getFloatValue(String what, AspSyntax where){
+		return this.floatValue;
+	}
+
+
+  @Override
+  protected String typeName() {
+    return "float";
+  }
+
+
+  @Override
+  public String toString() {
+    return Double.toString(this.floatValue);
+  }
+
+  @Override
+	public RuntimeValue evalAdd(RuntimeValue v, AspSyntax where){
+		if (v instanceof RuntimeFloatValue){
+			return new RuntimeFloatValue(v.getFloatValue("float", where) + this.floatValue);
+		}
+		else if(v instanceof RuntimeIntValue){
+			return new RuntimeFloatValue(v.getIntValue("float",where) + this.floatValue);
+		}
+		else{
+			runtimeError("Type error: "+ v +" of " + v.typeName() + " cannot be added to float!", where);
+		}
+		return null;
+	}
+
+	@Override
+	public RuntimeValue evalSubtract(RuntimeValue v, AspSyntax where){
+		if (v instanceof RuntimeFloatValue ){
+			return  new RuntimeFloatValue(this.floatValue - v.getFloatValue("float",where));
+		}
+		else if (v instanceof RuntimeIntValue){
+			return  new RuntimeFloatValue(this.floatValue - v.getIntValue("float",where));
+		}
+		else{
+			runtimeError("Type error: "+ v +" of " + v.typeName() + " cannot be subtracted from float!", where);
+		}
+		return null;
+	}
+
+	public RuntimeValue evalMultiply(RuntimeValue v, AspSyntax where){
+		if (v instanceof RuntimeFloatValue ){
+			return  new RuntimeFloatValue(this.floatValue * v.getFloatValue("float",where));
+		}
+		else if (v instanceof RuntimeIntValue){
+			return  new RuntimeFloatValue(this.floatValue * v.getIntValue("float",where));
+		}
+		else{
+			runtimeError("Type error: "+ v +" of " + v.typeName() + " cannot be multiply to float!", where);
+		}
+		return null;
+	}
+
+	public RuntimeValue evalLess(RuntimeValue v, AspSyntax where){
+		if (v instanceof RuntimeFloatValue ){
+			return  new RuntimeBoolValue(this.floatValue < v.getFloatValue("float",where));
+		}
+		else if (v instanceof RuntimeIntValue){
+			return  new RuntimeBoolValue(this.floatValue < v.getIntValue("float",where));
+		}
+		else{
+			runtimeError("Type error: "+ v +" of " + v.typeName() + " cannot be compared to float!", where);
+		}
+		return null;
+	}
+
+	public RuntimeValue evalGreater(RuntimeValue v, AspSyntax where){
+		if (v instanceof RuntimeFloatValue ){
+			return  new RuntimeBoolValue(this.floatValue > v.getFloatValue("float",where));
+		}
+		else if (v instanceof RuntimeIntValue){
+			return  new RuntimeBoolValue(this.floatValue > v.getIntValue("float",where));
+		}
+		else{
+			runtimeError("Type error: "+ v +" of " + v.typeName() + " cannot be compared to float!", where);
+		}
+		return null;
+	}
+
+}
