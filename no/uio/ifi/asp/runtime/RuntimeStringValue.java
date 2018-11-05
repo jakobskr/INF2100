1be3513c16462e36d83f953e54b6374e0fe56d42
Jakob Skrede <jakob2407@gmail.com>
2018-11-05 14:35:31 +0100

    sick

--- no/uio/ifi/asp/runtime/RuntimeStringValue.java
+++ no/uio/ifi/asp/runtime/RuntimeStringValue.java
@@ -29,13 +29,109 @@ public class RuntimeStringValue extends RuntimeValue {
 
   public RuntimeValue evalAdd(RuntimeValue v, AspSyntax where) {
     if (v instanceof RuntimeStringValue) {
-      //return new RuntimeStringValue(value + v.getStringValue("string", where));
+      return new RuntimeStringValue(value + v.getStringValue("string", where));
     }
 
     runtimeError("Type error for +", where);
     return null;
   }
 
+  @Override
+  public RuntimeValue evalEqual(RuntimeValue v, AspSyntax where) {
+    if (v instanceof RuntimeNoneValue) {
+      return new RuntimeBoolValue(false);
+    }
 
+    if (v instanceof RuntimeStringValue) {
+      return new RuntimeBoolValue(value.equals(v.getStringValue("string", where)));
+    }
 
+    runtimeError("Type error for ==.", where);
+    return null;  // Required by the compiler
+  }
+
+  @Override
+  public RuntimeValue evalGreater(RuntimeValue v, AspSyntax where) {
+
+    if (v instanceof RuntimeStringValue) {
+      return new RuntimeBoolValue(value.compareTo(v.getStringValue("string", where)) > 0 );
+    }
+
+    runtimeError("Type error for <.", where);
+    return null;  // Required by the compiler
+  }
+
+  @Override
+  public RuntimeValue evalGreaterEqual(RuntimeValue v, AspSyntax where) {
+
+    if (v instanceof RuntimeStringValue) {
+      return new RuntimeBoolValue(value.compareTo(v.getStringValue("string", where)) >= 0 );
+    }
+
+    runtimeError("Type error for <.", where);
+    return null;  // Required by the compiler
+  }
+
+  public RuntimeValue evalLen(RuntimeValue v, AspSyntax where) {
+    return new RuntimeIntValue(value.length());
+  }
+
+  @Override
+  public RuntimeValue evalLess(RuntimeValue v, AspSyntax where) {
+
+    if (v instanceof RuntimeStringValue) {
+      return new RuntimeBoolValue(value.compareTo(v.getStringValue("string", where)) < 0 );
+    }
+
+    runtimeError("Type error for <.", where);
+    return null;  // Required by the compiler
+  }
+
+  @Override
+  public RuntimeValue evalLessEqual(RuntimeValue v, AspSyntax where) {
+
+    if (v instanceof RuntimeStringValue) {
+      return new RuntimeBoolValue(value.compareTo(v.getStringValue("string", where)) <= 0 );
+    }
+
+    runtimeError("Type error for <.", where);
+    return null;  // Required by the compiler
+  }
+
+  public RuntimeValue evalMultiply(RuntimeValue v, AspSyntax where) {
+    if (v instanceof RuntimeIntValue) {
+      String ret = "";
+      for (int i = 0; i < v.getIntValue("integer",where) ;i++ ) {
+        ret = ret + ret;
+      }
+      return new RuntimeStringValue(ret);
+    }
+
+    runtimeError("Type error for +", where);
+    return null;
+  }
+
+  public boolean evalNot(RuntimeValue v, AspSyntax where) {
+    return (value.equals(""));
+  }
+
+  @Override
+  public RuntimeValue evalNotEqual(RuntimeValue v, AspSyntax where) {
+    if (v instanceof RuntimeNoneValue) {
+      return new RuntimeBoolValue(true);
+    }
+
+    if (v instanceof RuntimeStringValue) {
+      return new RuntimeBoolValue(!(value.equals(v.getStringValue("string", where))));
+    }
+
+    runtimeError("Type error for !=.", where);
+    return null;  // Required by the compiler
+  }
+
+  public RuntimeValue evalSubscription(RuntimeValue v, AspSyntax where) {
+	  if (v instanceof int) {
+
+    }
+  }
 }
